7febdefec78780e7f1f25b423313912be2a9e63b
HemanthVadde <56255429+HemanthVadde@users.noreply.github.com>
2021-07-28 13:04:19 +0530

    Update __init__.py

--- DynamicDatabaseCon/__init__.py
+++ DynamicDatabaseCon/__init__.py
@@ -4,14 +4,15 @@ from sqlalchemy.engine import create_engine
 def create_dbengine(dbtype,host,username,password,dbname):
 	db = dbtype.lower()
 	if db == 'oracle':
-		engine_str = "{db} + cx_oracle + ://{username}:{password}@{host}/?service_name={dbname}"
+		engine_str = f"{db}+cx_oracle://{username}:{password}@{host}/?service_name={dbname}"
 	elif db == 'postgresql':
-		engine_str = "{db}}+psycopg2://{username}:{password}@{host}/{dbname}"
+		engine_str = f"{db}+psycopg2://{username}:{password}@{host}/{dbname}"
 	elif db == 'mssql':
-		engine_str = "{db}}+pyodbc://{username}:{password}@{host}/{dbname}"
+		engine_str = f"{db}+pyodbc://{username}:{password}@{host}/{dbname}"
 	elif db == 'mysql':
-		engine_str = "{db}}+pymysql://{username}:{password}@{host}/{dbname}?charset=utf8mb4"
+		engine_str = f"{db}+pymysql://{username}:{password}@{host}/{dbname}?charset=utf8mb4"
 	else:
 		raise EXCEPTION("Please pass propert database types. Currently supports only oracle, postgresql, mssql & mysql")
+		return
 	engine = create_engine(engine_str)
 	return engine
